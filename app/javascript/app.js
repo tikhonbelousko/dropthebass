0afbc87414e971ba87a4b1040206a35478caeefc
Tikhon Belousko <dazmail01@gmail.com>
2015-11-16 12:56:18 +0200

    Add analyzer.

--- app/javascript/app.js
+++ app/javascript/app.js
@@ -2,7 +2,7 @@ import $ from 'jquery'
 
 // Constants
 const CLIENT_ID = '61b17ec61bd38e4b6cfe078e58d5a062'
-const TRACK_URL = 'https://soundcloud.com/muse/undisclosed-desires'
+const TRACK_URL = 'https://soundcloud.com/odesza/say_my_name'
 
 // On Load
 document.addEventListener('DOMContentLoaded', onLoad, false)
@@ -11,8 +11,21 @@ document.addEventListener('DOMContentLoaded', onLoad, false)
 function onLoad() {
   // Player
   let player =  document.getElementById('player')
-  // let audioContext = new (window.AudioContext || window.webkitAudioContext)
-  // let source = audioContext.createMediaElementSource(player)
+  player.crossOrigin = 'Anonymous'
+
+  let audioContext = new (window.AudioContext || window.webkitAudioContext)
+  let source = audioContext.createMediaElementSource(player)
+  source.connect(audioContext.destination)
+
+  let analyser = audioContext.createAnalyser()
+
+  source.connect(analyser)
+
+  // Update equalizer
+  setInterval(() => {
+    let streamData = new Uint8Array(128)
+    analyser.getByteFrequencyData(streamData)
+  }, 100)
 
   SC.initialize({
     client_id: CLIENT_ID,
@@ -23,6 +36,7 @@ function onLoad() {
       let streamUrl = `${sound.stream_url}?client_id=${CLIENT_ID}`
       player.setAttribute('src', streamUrl)
       player.play()
+      //
     }
   })
 }
