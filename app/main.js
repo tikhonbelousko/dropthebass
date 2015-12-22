c3313ce6795a64e8967f3cfdf6765a5e6b674498
Tikhon Belousko <dazmail01@gmail.com>
2015-12-22 18:37:46 +0300

    Add gl3.js demo.

--- app/main.js
+++ app/main.js
@@ -1,3 +1,5 @@
 import './styles/main.styl'
-//import './javascript/app.js'
-import './javascript/gl2.js'
+// import './javascript/app.js'
+// import './javascript/gl1.js'
+// import './javascript/gl2.js'
+import './javascript/gl3.js'
