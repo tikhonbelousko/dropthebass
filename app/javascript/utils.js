9b7acb1e51a17e68952bd967cb8b960e7690a6f2
Tikhon Belousko <dazmail01@gmail.com>
2015-12-24 14:53:25 +0300

    Start doing equalizer.

---
+++ app/javascript/utils.js
@@ -0,0 +1,40 @@
+import { mat4 } from 'gl-matrix'
+
+// ---
+// Init WebGL with canvas
+// ---
+export function initWebGL(canvas) {
+  let gl = null
+
+  try {
+    gl = canvas.getContext('webgl') || canvas.getContext('experimental-webgl')
+  } catch(e) {}
+
+  if (!gl) {
+    console.log('Unable to initialize WebGL. Your browser may not support it.')
+    gl = null
+  }
+
+  return gl
+}
+
+// ---
+// Request animation frame
+// ---
+let requestAnimFrame = (function() {
+  return window.requestAnimationFrame ||
+         window.webkitRequestAnimationFrame ||
+         window.mozRequestAnimationFrame ||
+         window.oRequestAnimationFrame ||
+         window.msRequestAnimationFrame ||
+         ((callback, element) => window.setTimeout(callback, 1000/60));
+})()
+
+
+// ---
+// Repeat animation
+// ---
+export function repeatAnimation(callback) {
+  callback()
+  setTimeout( () => requestAnimFrame(repeatAnimation.bind(this, callback)), 1000 / 60 )
+}
